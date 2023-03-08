a0710c1d6f5d78e0abc3d41bb7f28f55e4fe0fd7
creatorQuen <spastime@yandex.ru>
2023-03-08 16:11:05 +0300

    feat: add class to fetch by api

--- src/index.js
+++ src/index.js
@@ -1,24 +1,84 @@
 console.log('Hello World');
 
-const getResource = async (url) => {
-  const res = await fetch(url);
+class StarWarsService {
 
-  if (!res.ok) {
-    throw new Error(`N00000t fetch ${url}`, `received ${res.status}`);
-  }
+  _apiBase = 'https://swapi.dev/api';
 
-  const body = await res.json();
-  // arrayBuffer(), blob(), text(), formData() 
-  return body;
-};
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
 
-getResource('https://swapi.dev/api/people/123232323')
-  .then((body) => {
-    console.log(body);
-  })
-  .catch((err) => {
-    console.error('Not fetch', err);
+    if (!res.ok) {
+      throw new Error(`Must not fetch ${url}`, `received ${res.status}`);
+    }
+  
+    const body = await res.json();
+    return body;
+  };
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results
+  };
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  };
+
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results
+  };
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`);
+  };
+
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results
+  };
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`);
+  };
+
+}
+
+const swApi = new StarWarsService();
+
+swApi.getAllPeople().then((people) => {
+  people.forEach((p) => {
+    console.log(p.name);
   });
+});
+
+swApi.getPerson(7).then((person) => {
+  console.log('\n', person.name);
+})
+
+
+////-------------------------------------
+// const getResource = async (url) => {
+//   const res = await fetch(url);
+
+//   if (!res.ok) {
+//     throw new Error(`N00000t fetch ${url}`, `received ${res.status}`);
+//   }
+
+//   const body = await res.json();
+//   // arrayBuffer(), blob(), text(), formData() 
+//   return body;
+// };
+
+// getResource('https://swapi.dev/api/people/123232323')
+//   .then((body) => {
+//     console.log(body);
+//   })
+//   .catch((err) => {
+//     console.error('Not fetch', err);
+//   });
 
 
 
