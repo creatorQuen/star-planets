63a2ae769ce84d4da51381df031490408c9bba0f
creatorQuen <spastime@yandex.ru>
2023-03-08 15:35:42 +0300

    feat: add catch to fetch

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ console.log('Hello World');
 
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`N00000t fetch ${url}`, `received ${res.status}`);
+  }
+
   const body = await res.json();
   // arrayBuffer(), blob(), text(), formData() 
   return body;
 };
 
-getResource('https://swapi.dev/api/people/1')
+getResource('https://swapi.dev/api/people/123232323')
   .then((body) => {
     console.log(body);
+  })
+  .catch((err) => {
+    console.error('Not fetch', err);
   });
 
 
