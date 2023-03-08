da3da086785a39fea45f2c76c9df5f0d9ef6db20
creatorQuen <spastime@yandex.ru>
2023-03-08 15:26:56 +0300

    feat: fetch data by api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 console.log('Hello World');
+
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  // arrayBuffer(), blob(), text(), formData() 
+  return body;
+};
+
+getResource('https://swapi.dev/api/people/1')
+  .then((body) => {
+    console.log(body);
+  });
+
+
+
+////------------------------------------------
+// fetch('https://swapi.dev/api/people/1')
+//   .then((res) => {
+//     console.log('Got Response', res.status);
+//     return res.json();
+//   })
+//   .then((body) => {
+//     console.log(body);
+//   });
+
